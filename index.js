5fe7b9b22556c381b9fb1278b7d1e8cd3b2dc8de
Kai Wang <kai.wang.nankai@gmail.com>
2023-04-26 10:38:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+router.get("/data", (_, res) => {
+  res.send({ key1: "value1", key2: "value2" });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => console.log("Server is running on Port: " + PORT));
